41e7aa5a52133e6cc4d94c3af54761d01d7eacf3
Nicholas Chao <nibchao@gmail.com>
2022-12-04 13:43:27 -0800

    slide 57

--- main.cpp
+++ main.cpp
@@ -24,41 +24,45 @@ int main()
 			fileSize++;
 			break;
 		}
-		
 	}
-
-	/* get first number in each line ("class" for each feature)
-	while (getline(dataFile, line))
-	{
-		stringstream lineStream(line);
-		float a;
-		while (lineStream >> a)
-		{
-			cout << a << " ";
-			break;
-		}
-		cout << "\n";
-	}*/
-
+	dataFile.clear();
+	dataFile.seekg(0, dataFile.beg);
 	for (int i = 497; i <= fileSize; i++)
 	{
-		cout << "On the " << i << "th level of the search tree\n";
-		vector<int> featureToAddAtThisLevel;
-		int bestSoFarAccuracy = 0;
-		for (int k = 497; k <= fileSize; k++)
-		{
-			if (currentSetOfFeatures.empty())
-			{
-				cout << "--Considering adding the " << k << " feature\n";
-				int accuracy = crossValidation(dataFile, currentSetOfFeatures, k + 1);
-				if (accuracy > bestSoFarAccuracy)
-				{
-					bestSoFarAccuracy = accuracy;
-					featureToAddAtThisLevel.push_back(k);
-				}
-			}
-		}
+		getline(dataFile, line);
+		stringstream lineStream(line);
+		float a;
+		lineStream >> a;
+		cout << "Looping over i, at the " << i << " location\n";
+		cout << "The " << i << "th object is in class " << a << "\n";
 	}
+
+	//for (int i = 497; i <= fileSize; i++)
+	//{
+	//	cout << "On the " << i << "th level of the search tree\n";
+	//	int featureToAddAtThisLevel;
+	//	int bestSoFarAccuracy = 0;
+	//	bool stop = false;
+	//	for (int k = 497; k <= fileSize; k++)
+	//	{
+	//		if (count(currentSetOfFeatures.begin(), currentSetOfFeatures.end(), k))
+	//		{
+	//			break; // feature already added, skip
+	//		}
+	//		else
+	//		{
+	//			cout << "--Considering adding the " << k << " feature\n";
+	//			int accuracy = crossValidation(dataFile, currentSetOfFeatures, k + 1);
+	//			if (accuracy > bestSoFarAccuracy)
+	//			{
+	//				bestSoFarAccuracy = accuracy;
+	//				featureToAddAtThisLevel = k;
+	//			}
+	//		}		
+	//	}
+	//	currentSetOfFeatures.push_back(featureToAddAtThisLevel);
+	//	cout << "On level " << i << " I added feature " << featureToAddAtThisLevel << " to current set\n";
+	//}
 	return 0;
 }
 
