7eb39f89d6b0b12a565632ad6cdda0d4fedc8046
Nicholas Chao <nibchao@gmail.com>
2022-12-05 16:38:37 -0800

    added calculation of euclidean distance for nearest neighbor

--- main.cpp
+++ main.cpp
@@ -6,24 +6,22 @@
 
 using namespace std;
 
-int crossValidation(ifstream&, vector<int>, int);
-double nearestNeighbor(vector<vector<double>>);
-void searchAlgorithm(int);
-
-const int TEMP_DATA_LIMIT_CONST = 495; // temp line for testing
-
 struct datasetPoint
 {
 	int dataClass;
 	vector<double> dataFeatures;
 };
 
+double nearestNeighbor(vector<datasetPoint>, vector<int>);
+void searchAlgorithm(int);
+double calculateDistance(datasetPoint, datasetPoint, vector<int>);
+
 int main()
 {
 	string fileName = "";
 	int algorithmInput = 0;
 
-	int row = 0; int column = 0;
+	int row = 0; int column = -1;
 	int dataClass = 0; double a = 0;
 	string line = "";
 
@@ -38,8 +36,10 @@ int main()
 	cout << "2) Backward Elimination\n";
 	cin >> algorithmInput;
 	ifstream dataFile(fileName);*/
-	ifstream dataFile("CS170_Small_Data__103.txt"); // temp line for testing
-	
+
+	//ifstream dataFile("CS170_Small_Data__103.txt"); // temp line for testing my data
+	ifstream dataFile("CS170_Small_Data__96.txt"); // temp line for testing sue data
+
 	while (getline(dataFile, line))
 	{
 		row++;
@@ -64,75 +64,62 @@ int main()
 			dataInEachLine.dataFeatures.clear();
 		}
 	}
-	column--;
-	//cout << "\nThis dataset has " << column << " features (not including the class attribute), with " << dataVector.size() << " instances.\n";
+	vector<int> currentSetOfFeatures;
+	for (int cnt = 0; cnt < column; cnt++)
+	{
+		currentSetOfFeatures.push_back(cnt + 1);
+	}
+	//cout << "\nThis dataset has " << column << " features (not including the class attribute), with " << dataVector.size() << " instances.\n\n";
+	cout << "Running nearest neighbor with all " << column << " features, using 'leaving-one-out' evaluation, I get an accuracy of " << nearestNeighbor(dataVector, currentSetOfFeatures)*100 << "%\n\n";
+	cout << "Beginning search.\n";
 	searchAlgorithm(algorithmInput);
-	//nearestNeighbor(dataVector);
 	return 0;
 }
 
-double nearestNeighbor(vector<vector<double>> dataVector)
+double nearestNeighbor(vector<datasetPoint> dataVector, vector<int> currentSetOfFeatures)
 {
-	vector<int> currentSetOfFeatures;
-	int numberCorrectlyClassified = 0;
+	double numberCorrectlyClassified = 0;
+	datasetPoint leaveOneOut;
 
-	for (int i = 1; i <= dataVector.size() - TEMP_DATA_LIMIT_CONST; i++)
+	for (int i = 0; i < dataVector.size(); i++)
 	{
-		double labelObjectToClassify = dataVector[i][0];
+		datasetPoint objectToClassify = dataVector.at(i); // refers to an entire line (object) in dataVector
+		double labelObjectToClassify = dataVector.at(i).dataClass;
+
 		double nearestNeighborDistance = INT_MAX;
 		double nearestNeighborLocation = INT_MAX;
-		double nearestNeighborLabel = 0;
-		for (int k = 1; k <= dataVector.size() - TEMP_DATA_LIMIT_CONST; k++)
+		double nearestNeighborLabel = -1;
+		for (int k = 0; k < dataVector.size(); k++)
 		{
 			if (i != k)
 			{			
-				double distance = 0; // replace 0 with sqrt(sum(objectToClassify - data(k,2:end)).^2))
+				double distance = calculateDistance(objectToClassify, dataVector.at(k), currentSetOfFeatures);
 				if (distance < nearestNeighborDistance)
 				{
 					nearestNeighborDistance = distance;
 					nearestNeighborLocation = k;
-					nearestNeighborLabel = dataVector[nearestNeighborLocation][0]; // this is supposed to refer to first column class label for this specified nearest neighbor or data(nearestNeighborLocation,1)
+					nearestNeighborLabel = dataVector.at(k).dataClass;
 				}
 			}
 		}
-		if (labelObjectToClassify == nearestNeighborLabel)
+		if (objectToClassify.dataClass == nearestNeighborLabel)
 		{
 			numberCorrectlyClassified++;
 		}
 		cout << "Object " << i << " is class " << labelObjectToClassify << "\n";
-		cout << "Its nearest_neighbor is " << nearestNeighborLocation << " which is in class " << labelObjectToClassify << "\n\n";
+		cout << "Its nearest_neighbor is " << nearestNeighborLocation << " which is in class " << nearestNeighborLabel << "\n\n";
 	}
 	return numberCorrectlyClassified / dataVector.size();
 }
 
-void searchtemp()
+double calculateDistance(datasetPoint line, datasetPoint line2, vector<int> currentSetOfFeatures)
 {
-	//for (int i = 1; i <= fileSize - TEMP_DATA_LIMIT_CONST; i++)
-	//{
-	//	cout << "On the " << i << "th level of the search tree\n";
-	//	int featureToAddAtThisLevel;
-	//	int bestSoFarAccuracy = 0;
-	//	bool stop = false;
-	//	for (int k = 1; k <= fileSize - TEMP_DATA_LIMIT_CONST; k++)
-	//	{
-	//		if (count(currentSetOfFeatures.begin(), currentSetOfFeatures.end(), k))
-	//		{
-	//			break; // feature already added, skip
-	//		}
-	//		else
-	//		{
-	//			cout << "--Considering adding the " << k << " feature\n";
-	//			int accuracy = crossValidation(dataFile, currentSetOfFeatures, k + 1);
-	//			if (accuracy > bestSoFarAccuracy)
-	//			{
-	//				bestSoFarAccuracy = accuracy;
-	//				featureToAddAtThisLevel = k;
-	//			}
-	//		}		
-	//	}
-	//	currentSetOfFeatures.push_back(featureToAddAtThisLevel);
-	//	cout << "On level " << i << " I added feature " << featureToAddAtThisLevel << " to current set\n";
-	//}
+	double distance = 0;
+	for (int cnt = 0; cnt < currentSetOfFeatures.size(); cnt++)
+	{
+		distance += pow(line.dataFeatures.at(currentSetOfFeatures.at(cnt)) - line2.dataFeatures.at(currentSetOfFeatures.at(cnt)), 2);
+	}
+	return pow(distance, 0.5);
 }
 
 void searchAlgorithm(int algorithmInput)
@@ -140,15 +127,61 @@ void searchAlgorithm(int algorithmInput)
 	if (algorithmInput == 1)
 	{
 		// do forward selection
+		//for (int i = 1; i <= fileSize; i++)
+		//{
+		//	cout << "On the " << i << "th level of the search tree\n";
+		//	int featureToAddAtThisLevel;
+		//	int bestSoFarAccuracy = 0;
+		//	bool stop = false;
+		//	for (int k = 1; k <= fileSize; k++)
+		//	{
+		//		if (count(currentSetOfFeatures.begin(), currentSetOfFeatures.end(), k))
+		//		{
+		//			break; // feature already added, skip
+		//		}
+		//		else
+		//		{
+		//			cout << "--Considering adding the " << k << " feature\n";
+		//			int accuracy = crossValidation(dataFile, currentSetOfFeatures, k + 1);
+		//			if (accuracy > bestSoFarAccuracy)
+		//			{
+		//				bestSoFarAccuracy = accuracy;
+		//				featureToAddAtThisLevel = k;
+		//			}
+		//		}		
+		//	}
+		//	currentSetOfFeatures.push_back(featureToAddAtThisLevel);
+		//	cout << "On level " << i << " I added feature " << featureToAddAtThisLevel << " to current set\n";
+		//}
 	}
 	else if (algorithmInput == 2)
 	{
 		// do backward elimination
+		//for (int i = 1; i <= fileSize; i++)
+		//{
+		//	cout << "On the " << i << "th level of the search tree\n";
+		//	int featureToAddAtThisLevel;
+		//	int bestSoFarAccuracy = 0;
+		//	bool stop = false;
+		//	for (int k = 1; k <= fileSize; k++)
+		//	{
+		//		if (count(currentSetOfFeatures.begin(), currentSetOfFeatures.end(), k))
+		//		{
+		//			break; // feature already added, skip
+		//		}
+		//		else
+		//		{
+		//			cout << "--Considering adding the " << k << " feature\n";
+		//			int accuracy = crossValidation(dataFile, currentSetOfFeatures, k + 1);
+		//			if (accuracy > bestSoFarAccuracy)
+		//			{
+		//				bestSoFarAccuracy = accuracy;
+		//				featureToAddAtThisLevel = k;
+		//			}
+		//		}		
+		//	}
+		//	currentSetOfFeatures.push_back(featureToAddAtThisLevel);
+		//	cout << "On level " << i << " I added feature " << featureToAddAtThisLevel << " to current set\n";
+		//}
 	}
-}
-
-int crossValidation(ifstream& dataFile, vector<int> currentSetOfFeatures, int featureToAdd)
-{
-	srand(time(0));
-	return rand() % 100;
 }
